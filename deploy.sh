e02183557dffde0c4bf016a807a43ccdc91ae234
Otmar Ertl <otmar.ertl@gmail.com>
2016-09-03 23:25:15 +0200

    3rd attempt to fix deployment script, fixed wrong folder

--- deploy.sh
+++ deploy.sh
@@ -20,25 +20,25 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into gh-pages/
+# Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO gh-pages
-cd gh-pages
+git clone $REPO out
+cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf gh-pages/**/* || exit 0
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
-cd gh-pages
+cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled gh-pages (e.g. this is a README update) then just bail.
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
